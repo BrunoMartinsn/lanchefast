4ae1cfd80568094542c5f35d88724c8467b67813
bruno <brurro67@gmail.com>
2025-04-10 15:25:58 -0300

    implementação do crud de show, edit, create

--- app/Livewire/Clientes/Create.php
+++ app/Livewire/Clientes/Create.php
@@ -2,10 +2,57 @@
 
 namespace App\Livewire\Clientes;
 
+use App\Models\Cliente;
+use Illuminate\Support\Facades\Hash;
 use Livewire\Component;
 
 class Create extends Component
 {
+    public $nome;
+    public $endereco;
+    public $telefone;
+    public $cpf;
+    public $email;
+    public $senha;
+
+    protected $rules = [
+        'nome'=> 'required',
+        'endereco' => 'required',
+        'telefone' => 'required',
+        'cpf' => 'required|min:11|unique',
+        'email' => 'required|email|unique',
+        'senha' => 'required|min:6'
+    ];
+
+    protected $messages = [
+        'nome.required' => 'nome obrigatorio',
+        'endereco.required' => 'endereco obrigatorio',
+        'telefone.required' => 'telefone obrigatório',
+        'cpf.required' => 'cpf obrigatório',
+        'cpf.unique' => 'cpf unico',
+        'cpf.min' => 'cpf deve conter no minimo 11 caracteres',
+        'email.require' => 'email obrigatorio',
+        'email.unique' => 'email unico',
+        'email.email' => 'formato de email invalido',
+        'senha.required' => 'senha obrigatório',
+        'senha.min' => 'senha deve conter no minimo 6 caracteres'
+    ];
+    
+
+    public function store(){
+        Cliente::create([
+            'nome' => $this->nome,
+            'endereco' => $this->endereco,
+            'telefone' => $this->telefone,
+            'cpf' => $this->cpf,
+            'email' => $this->email,
+            'senha' => Hash::make($this->senha),
+
+        ]); 
+
+        session()->flash('success', 'Cadastro Realizado');
+        $this->validate();
+    }
     public function render()
     {
         return view('livewire.clientes.create');
--- resources/views/livewire/clientes/show.blade.php
+++ resources/views/livewire/clientes/show.blade.php
@@ -1,3 +1,85 @@
+
 <div>
-    {{-- Care about people's approval and you will be their prisoner. --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+
+<body class="text-bg-secondary p-3">
+
+
+
+    
+
+
+
+    <div class="container d-flex justify-content-center" style="width: 39%">
+        <div class="row">
+
+            <div class="row">
+
+
+                <div class="row align-items-center">
+
+                    <div class="col">
+                        <div class="card  bor" style="width: 30rem;" class="mb-10">
+                            <div class="card-body">
+                                <table class="table table-bordered table-hover">
+                                    <tbody>
+                                        <tr>
+                                            <th class="fw-bold">Nome</th>
+                                            <td>{{ $cliente->nome }}</td>
+                                        </tr>
+                                        <tr>
+                                            <th class="fw-bold">Endereço</th>
+                                            <td>{{ $cliente->endereco }}</td>
+                                        </tr>
+                                        <tr>
+                                            <th class="fw-bold">Telefone</th>
+                                            <td>{{ $cliente->telefone }}</td>
+                                        </tr>
+                                        <tr>
+                                            <th class="fw-bold">CPF</th>
+                                            <td>{{ $cliente->cpf }}</td>
+                                        </tr>
+                                        <tr>
+                                            <th class="fw-bold">Email</th>
+                                            <td>{{ $cliente->email }}</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+
+
+                                            </div>
+                                            <div class="card-footer text-end">
+                                                <a href="{{ route('clientes.index') }}" class="btn btn-secondary">Voltar</a>
+                                            </div>
+                                        </form>
+
+                                    </div>
+                                </div>
+
+
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+
+
+
+            </div>
+
+            
+
+
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+                integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+            </script>
+</body>
+
 </div>
+</div>
+</div>
+
