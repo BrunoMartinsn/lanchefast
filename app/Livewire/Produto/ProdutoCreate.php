57105a86ac72913868911eeab5092450ee997203
bruno <brurro67@gmail.com>
2025-04-10 15:44:13 -0300

    implimentaçao de create, edit, show de produto

--- app/Livewire/Produto/ProdutoCreate.php
+++ app/Livewire/Produto/ProdutoCreate.php
@@ -2,10 +2,46 @@
 
 namespace App\Livewire\Produto;
 
+use App\Models\Produto;
+use Illuminate\Support\Facades\Hash;
 use Livewire\Component;
 
 class ProdutoCreate extends Component
 {
+    public $nome;
+    public $ingredientes;
+    public $valor;
+    public $imagem;
+    
+
+    protected $rules = [
+        'nome'=> 'required',
+        'ingredientes' => 'required',
+        'valor' => 'required|decimal 10,2',
+        
+    ];
+
+    protected $messages = [
+        'nome.required' => 'nome obrigatorio',
+        'ingredientes.required' => 'ingredientes obrigatorio',
+        'valor.required' => 'valor obrigatório',
+        'valor.decimal' => 'valor decimal 8,2'
+    ];
+    
+
+    public function store(){
+        Produto::create([
+            'nome' => $this->nome,
+            'ingredientes' => $this->ingredientes,
+            'valor' => $this->valor,
+            
+
+        ]); 
+
+        session()->flash('success', 'Cadastro Realizado');
+        $this->validate();
+    }
+    
     
     public function render()
     {
